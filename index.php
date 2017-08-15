abe2ee61adf42aa9aef33ee0e43d128d9698d287
castle <nobu.castle@gmail.com>
2017-08-15 11:55:53 +0900

    update sql2

--- index.php
+++ index.php
@@ -2,6 +2,19 @@
 
 $url = parse_url(getenv('ec2-54-227-252-202.compute-1.amazonaws.com:5432/d83v5gtterpnj3'));
 
+return [
+    'default' => 'pgsql',
+
+    'connections' => [
+        'pgsql' => [
+            'data_id' => $url['Id'],
+            'giinname' => $url['Name'],
+            'giinkana' => $url['name_kana__c'],
+            'e_mail' => $url['e_mail__c'],
+        ],
+    ],
+];
+
 ?> 
 
 

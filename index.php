f8d935f1a909cb23b1cadc81e8295655bb2c67f1
castle <nobu.castle@gmail.com>
2017-08-18 15:43:28 +0900

    change16

--- index.php
+++ index.php
@@ -10,12 +10,17 @@ function pg_connection_string_from_database_url() {
 $pg_conn = pg_connect(pg_connection_string_from_database_url());
 # Now let's use the connection for something silly just to prove it works:
 $result = pg_query($pg_conn, "SELECT name, name_kana__c, syubetsu__c FROM salesforce.tou_jinji__c where id = 1 ORDER BY systemmodstamp DESC");
+
 print "<pre>\n";
+
 if (!pg_num_rows($result)) {
-  print("Your connection is working, but your database is empty.\nFret not. This is expected for new apps.\n");
+	print("Your connection is working, but your database is empty.\nFret not. This is expected for new apps.\n");
 } else {
-  print "Tables in your database:\n";
-  while ($row = pg_fetch_row($result)) { print("- $row[0]\n"); }
+	print "Tables in your database:\n";
+	print("- $row[0]\n");
+	print("- $row[1]\n");
+	print("- $row[2]\n");
+	# while ($row = pg_fetch_row($result)) { print("- $row[0]\n"); }
 }
 print "\n";
 

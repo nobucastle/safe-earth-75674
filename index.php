ada905a14fb8e33c8e39e61ad6be524a821e5a3e
nobucastle <30787155+nobucastle@users.noreply.github.com>
2019-11-06 16:35:34 +0900

    3
    3

--- index.php
+++ index.php
@@ -5,6 +5,17 @@ use App\Controller\AppController;
 use Cake\Event\Event;
 use bjsmasth\Salesforce;
 
+class AccountsController extends AppController
+{
+	public $components = ["SFReset"]
+
+	public function beforeFilter(Event $event)
+	{
+		parent::beforeFilter($event);
+	}
+
+
+}
 
 ?> 
 

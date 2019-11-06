6a2261d80063c05c279ff7f1ce6a40249bb5c5a7
nobucastle <30787155+nobucastle@users.noreply.github.com>
2019-11-06 17:09:28 +0900

    10
    10

--- index.php
+++ index.php
@@ -9,8 +9,27 @@ class AccountsController extends AppController
 {
 	public $components = ["SFReset"];
 
+	public function beforeFilter(Event $event)
+	{
+		parent::beforeFilter($event);
+	}
 
+	public function index()
+	{
+		$this->autoRender = fales;
+
+		try{
+		    $query = 'SELECT Id, Name FROM ACCOUNT LIMIT 100';
+		    $crud = new Salesforce\CRUD();
+		    print_r("<pre>");
+		    print_r($crud->query($query));
+		    print_r("</pre>");
+		} catch (Exception $e) {
+		    echo '捕捉した例外: ', $e->getMessage(), "\n";
+		}
+	}
 }
+
 
 
 ?> 
